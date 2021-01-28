eccdd86dddb66a514999fbf161ec714711858b12
izereuwonkunda <izereuwonkundakelia@gmail.com>
2021-01-28 09:12:23 +0200

    footer topbrands and newsletter

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,10 @@ import Banner from "../components/homepage/Banner";
 import Home from "../components/homepage/Home";
 import Navbar from "../components/homepage/Navbar";
 import Title from "../components/homepage/Title";
-
+import Footer from '../components/homepage/Footer';
+import  Newsletter from '../components/homepage/Newsletter'
+import Stories from '../components/homepage/Stories'
+import Brands from '../components/homepage/Brands'
 export default function HomePage() {
   return (
     <div>
@@ -18,8 +21,16 @@ export default function HomePage() {
         <Title title="Promotions">
           <button>Child btn</button>
         </Title>
-        <Title title="What Our Customers Say" centered></Title>
+        <Title title="Top Brands"></Title>
+        <Brands/>
         <Title title="Why Us" centered></Title>
+        <Title title=" Our Satsfied Customers Says" centered></Title>
+        <Stories/>
+      
+
+
+         <Newsletter/>
+        <Footer/>
       </div>
     </div>
   );
