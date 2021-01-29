7ed3b3c0e56711c1bb3f3a62999403e76dd555d9
izereuwonkunda <izereuwonkundakelia@gmail.com>
2021-01-29 13:38:51 +0200

    stories updated

--- components/homepage/Stories.jsx
+++ components/homepage/Stories.jsx
@@ -1,55 +1,32 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Pagination } from "swiper";
+SwiperCore.use([Pagination]);
+const slides = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+   { id: 4 },
+  { id: 5 },
+    { id: 6 },
+ 
+ 
+]
+
 export default function Stories() {
   return (
-    <div className="grid grid-cols-2">
-      <div className=" m-20   p-2">
-        <div className=" w-1/3  ml-20 flex relative ">
-          <img
-            src="/img/avatar.png"
-            className="rounded-full  mb-2 h-36 w-36 flex items-center justify-center"
-          />
-          <div className="h-12 w-12  p-1 z-40 absolute bottom-0 left-24 bg-white rounded-full">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="35"
-              height="35"
-            >
-              <path fill="none" d="M0 0h24v24H0z" />
-              <path d="M4.583 17.321C3.553 16.227 3 15 3 13.011c0-3.5 2.457-6.637 6.03-8.188l.893 1.378c-3.335 1.804-3.987 4.145-4.247 5.621.537-.278 1.24-.375 1.929-.311 1.804.167 3.226 1.648 3.226 3.489a3.5 3.5 0 0 1-3.5 3.5c-1.073 0-2.099-.49-2.748-1.179zm10 0C13.553 16.227 13 15 13 13.011c0-3.5 2.457-6.637 6.03-8.188l.893 1.378c-3.335 1.804-3.987 4.145-4.247 5.621.537-.278 1.24-.375 1.929-.311 1.804.167 3.226 1.648 3.226 3.489a3.5 3.5 0 0 1-3.5 3.5c-1.073 0-2.099-.49-2.748-1.179z" />
-            </svg>
-          </div>
-        </div>
-        <div
-          className=" relative border-l border-black mt-20 h-36"
-          style={{ borderWidth: "0 0 0 1.5px" }}
-        >
-          <div
-            className="bg-black absolute "
-            style={{
-              left: "-2.5px",
-              bottom: "38px",
-              width: "3.3px",
-              height: "40%",
-            }}
-          ></div>
-          <p className="ml-12 p-4">
-            Far far away, behind the word mountains, far from the countries
-            Vokalia and Consonantia, there live the blind texts. Separated they
-            live inFar far away, behind the word mountains, far from the
-            countries{" "}
-          </p>
-        </div>
-        <div className="mt-16 bg-yello-600">
-          <p className="font-bold uppercase pt-2" style={{ fontSize: "18px" }}>
-            JOHN Smith MacBroom
-          </p>
-          <p className="uppercase  pt-2" style={{ fontSize: "15px" }}>
-            Marketing department
-          </p>
-        </div>
-      </div>
 
-      <div className=" m-20   p-2">
+    // <div >
+        <Swiper
+            spaceBetween={50}
+            slidesPerView={2}
+            pagination={{ clickable: true }}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+            className="grid grid-cols-2"
+          >
+{slides.map((slide) => (
+              <SwiperSlide key={slide.id}>
+              <div className=" m-20   p-2">
         <div className=" w-1/3  ml-20 flex relative ">
           <img
             src="/img/avatar.png"
@@ -96,100 +73,10 @@ export default function Stories() {
           </p>
         </div>
       </div>
-      <div className=" m-20   p-2">
-        <div className=" w-1/3  ml-20 flex relative ">
-          <img
-            src="/img/avatar.png"
-            className="rounded-full  mb-2 h-36 w-36 flex items-center justify-center"
-          />
-          <div className="h-12 w-12  p-1 z-40 absolute bottom-0 left-24 bg-white rounded-full">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="35"
-              height="35"
-            >
-              <path fill="none" d="M0 0h24v24H0z" />
-              <path d="M4.583 17.321C3.553 16.227 3 15 3 13.011c0-3.5 2.457-6.637 6.03-8.188l.893 1.378c-3.335 1.804-3.987 4.145-4.247 5.621.537-.278 1.24-.375 1.929-.311 1.804.167 3.226 1.648 3.226 3.489a3.5 3.5 0 0 1-3.5 3.5c-1.073 0-2.099-.49-2.748-1.179zm10 0C13.553 16.227 13 15 13 13.011c0-3.5 2.457-6.637 6.03-8.188l.893 1.378c-3.335 1.804-3.987 4.145-4.247 5.621.537-.278 1.24-.375 1.929-.311 1.804.167 3.226 1.648 3.226 3.489a3.5 3.5 0 0 1-3.5 3.5c-1.073 0-2.099-.49-2.748-1.179z" />
-            </svg>
-          </div>
-        </div>
-        <div
-          className=" relative border-l border-black mt-20 h-36"
-          style={{ borderWidth: "0 0 0 1.5px" }}
-        >
-          <div
-            className="bg-black absolute "
-            style={{
-              left: "-2.5px",
-              bottom: "38px",
-              width: "3.3px",
-              height: "40%",
-            }}
-          ></div>
-          <p className="ml-12 p-4">
-            Far far away, behind the word mountains, far from the countries
-            Vokalia and Consonantia, there live the blind texts. Separated they
-            live inFar far away, behind the word mountains, far from the
-            countries{" "}
-          </p>
-        </div>
-        <div className="mt-16 bg-yello-600">
-          <p className="font-bold uppercase pt-2" style={{ fontSize: "18px" }}>
-            JOHN Smith MacBroom
-          </p>
-          <p className="uppercase  pt-2" style={{ fontSize: "15px" }}>
-            Marketing department
-          </p>
-        </div>
-      </div>
-      <div className=" m-20   p-2">
-        <div className=" w-1/3  ml-20 flex relative ">
-          <img
-            src="/img/avatar.png"
-            className="rounded-full  mb-2 h-36 w-36 flex items-center justify-center"
-          />
-          <div className="h-12 w-12  p-1 z-40 absolute bottom-0 left-24 bg-white rounded-full">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="35"
-              height="35"
-            >
-              <path fill="none" d="M0 0h24v24H0z" />
-              <path d="M4.583 17.321C3.553 16.227 3 15 3 13.011c0-3.5 2.457-6.637 6.03-8.188l.893 1.378c-3.335 1.804-3.987 4.145-4.247 5.621.537-.278 1.24-.375 1.929-.311 1.804.167 3.226 1.648 3.226 3.489a3.5 3.5 0 0 1-3.5 3.5c-1.073 0-2.099-.49-2.748-1.179zm10 0C13.553 16.227 13 15 13 13.011c0-3.5 2.457-6.637 6.03-8.188l.893 1.378c-3.335 1.804-3.987 4.145-4.247 5.621.537-.278 1.24-.375 1.929-.311 1.804.167 3.226 1.648 3.226 3.489a3.5 3.5 0 0 1-3.5 3.5c-1.073 0-2.099-.49-2.748-1.179z" />
-            </svg>
-          </div>
-        </div>
-        <div
-          className=" relative border-l border-black mt-20 h-36"
-          style={{ borderWidth: "0 0 0 1.5px" }}
-        >
-          <div
-            className="bg-black absolute "
-            style={{
-              left: "-2.5px",
-              bottom: "38px",
-              width: "3.3px",
-              height: "40%",
-            }}
-          ></div>
-          <p className="ml-12 p-4">
-            Far far away, behind the word mountains, far from the countries
-            Vokalia and Consonantia, there live the blind texts. Separated they
-            live inFar far away, behind the word mountains, far from the
-            countries{" "}
-          </p>
-        </div>
-        <div className="mt-16 bg-yello-600">
-          <p className="font-bold uppercase pt-2" style={{ fontSize: "18px" }}>
-            JOHN Smith MacBroom
-          </p>
-          <p className="uppercase  pt-2" style={{ fontSize: "15px" }}>
-            Marketing department
-          </p>
-        </div>
-      </div>
-    </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+  
+    // </div>
   );
 }
