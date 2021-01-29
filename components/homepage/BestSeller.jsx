4b46991005c671c7d1e0390e4e1434b7608b9792
shallon kobusinge <kobshallon041@gmail.com>
2021-01-29 09:35:36 +0200

    best Seller implementation

--- components/homepage/BestSeller.jsx
+++ components/homepage/BestSeller.jsx
@@ -1,11 +1,34 @@
 import styles from "../../styles/BestSeller.module.css";
-
+import {useState,useEffect} from 'react'
+import { average } from "color.js";
 export default function BestSeller() {
+  const[colors,setColor ] = useState([]);
+  const [images] = useState([
+    '/img/kfc.jpg',
+    '/img/makeup.png',
+    '/img/watch.png',
+    '/img/sneakers.png'
+  ])
+
+  useEffect(() => {
+    async function getAverageColor(){
+     
+      for (let i = 0; i < images.length; i++) {
+        colors.push(await average(images[i], {format: "hex"}))
+      }
+      console.log(colors)
+      setColor(colors);
+    }
+    getAverageColor();
+ 
+  }, [images])
   return (
     <div className={styles.bestSeller}>
       <div >
         <img src="/img/kfc.jpg" alt="kfc" />
-        <div className={ styles.bestSellerDesc}>
+        <div className={ styles.bestSellerDesc}
+       style={{backgroundColor: colors[0]}}
+       >
         <h1 className="font-extrabold uppercase mb-2.5">KFC</h1>
         <p className="text-justify text-xs">
           The bestseller in food category Its popular and presentable in all.
@@ -15,7 +38,8 @@ export default function BestSeller() {
       </div>
       <div >
         <img src="/img/makeup.png" alt="makeup" />
-        <div  className={ styles.bestSellerDesc}>
+        <div  className={ styles.bestSellerDesc}
+        style={{backgroundColor: colors[1]}}>
         <h1 className="font-extrabold uppercase mb-2.5">Romantic Party</h1>
         <p className="text-justify text-xs">
           The bestseller in parfum category Its marvelous fragrance is lit
@@ -25,7 +49,8 @@ export default function BestSeller() {
       </div>
       <div>
         <img src="/img/watch.png" alt="watch" />
-        <div  className={ styles.bestSellerDesc}>
+        <div  className={ styles.bestSellerDesc}
+        style={{backgroundColor: colors[2]}}>
         <h1 className="font-extrabold uppercase mb-2.5">Apple Watch</h1>
         <p className="text-justify text-xs">
           The bestseller in watch category Its intelligent technology is legit.
@@ -35,7 +60,8 @@ export default function BestSeller() {
       </div>
       <div >
         <img src="/img/sneakers.png" alt="" />
-        <div className={ styles.bestSellerDesc}>
+        <div className={ styles.bestSellerDesc}
+        style={{backgroundColor: colors[3]}}>
         <h1 className="font-extrabold uppercase mb-2.5">Converse</h1>
         <p className="text-justify text-xs">
           The bestseller in shoes category Its popular and presentable in all.
