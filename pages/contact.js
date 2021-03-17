c68e23643e72459e4a955f5461857fac7baac2d2
Cyebukayire <peacecyebukayire@gmail.com>
2021-03-17 15:54:57 +0200

    Contact page Form

--- pages/contact.js
+++ pages/contact.js
@@ -4,10 +4,11 @@ import Head from "next/head";
 import Banner from "../components/homepage/Banner";
 import Title from "../components/homepage/Title";
 import styles from '../styles/Contact.module.css'
+import ContactSection from "../components/contactPage/ContactSection";
 
 export default function Contact(){
     return (
-        <div>
+        <div style={{backgroundColor: "#F1F1F1"}}>
             
       <Head>
         <title>Contact us | E-shop </title>
@@ -20,23 +21,7 @@ export default function Contact(){
             <h2 className="font-light text-lg text-gray-400 mt-4 text-center">Home &rarr; Contact Us</h2>
         </div>
 
-        {/* <div className="bg-white px-5 pt-8 md:px-12 md:py-16 lg:px-16 lg:py-20">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-0">
-                <div className={""}>
-                    <img loading={"lazy"} src="/img/about-us/about-us-girl-with-laptop.jfif" alt="Girl with laptop" className={"block w-full max-h-96"}/>
-                </div>
-                <div className="px-5 py-8 md:px-16 md:py-10 lg:px-20 lg:py-12">
-                    <h2 className="font-bold font-sans text-left text-3xl">Globally connected by large network</h2>
-                    <p className="font-bold mt-8">We are here to listen from you deliver excellence</p>
-                    <div className="desc">
-                        <p className="font-thin mt-10">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.
-                        </p>
-                        <button className="my-8 bg-black text-gray-300 font-bold rounded-lg py-3 px-7 hover:bg-gray-900">GET DETAILS</button>
-                    </div>
-                </div>
-            </div>
-        </div> */}
+        <ContactSection></ContactSection>
         
         <Footer />
         </div>
